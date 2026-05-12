f9a4bfd03b6a6519563ddbaae440b01bd2f02774
atik65 <atikhasan801@gmail.com>
2026-05-12 11:55:06 +0600

    add initial implementation of word reversal functions

---
+++ reverse-word-in-a-sentence/solve.ts
@@ -0,0 +1,25 @@
+const input = "a good   example";
+
+function reverse_words(s: string): string {
+  const words = s
+    .trim()
+    .split(" ")
+    .filter((word) => word.length > 0)
+    .map((word) => word.trim());
+  const reversed_words = words.reverse();
+
+  return reversed_words.join(" ");
+}
+
+console.log(reverse_words(input));
+
+// solution with regex to split with all whitespace
+function reverse_words_another(s: string): string {
+  const words = s
+    .trim()
+    .split(/\s+/)
+    .map((word) => word.trim());
+  const reversed_words = words.reverse();
+
+  return reversed_words.join(" ");
+}
