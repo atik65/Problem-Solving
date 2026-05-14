b068ca7172418dfca9328fcff0d7b874408f0d72
atik65 <atikhasan801@gmail.com>
2026-05-14 11:29:48 +0600

    feat: implement function to rotate a matrix 90 degrees clockwise

---
+++ Rotate a matrix 90 degrees clockwise/solve.ts
@@ -0,0 +1,27 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+
+const sample_matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+function rotate(matrix: number[][]): void {
+  // transponse of the matrix
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i + 1; j < matrix.length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  // reverse each row
+  for (let r = 0; r < matrix.length; r++) {
+    matrix[r] = matrix[r].reverse();
+  }
+}
+
+rotate(sample_matrix);
+
+console.log(sample_matrix);
